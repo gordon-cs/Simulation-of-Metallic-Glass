c72bdabdbc70443eb2072fad37e73a80c0d3c719
kuolu <kuolu1900@gmail.com>
2017-02-06 10:08:34 -0500

    Hello

---
+++ server/test/unitTest.cc
@@ -0,0 +1,19 @@
+/* Unit tests for server
+ * created by Kuo Lu
+ */
+
+#include "../controller.cc"
+#include "../src/readhdf5.h"
+
+using namespace std;
+int gPass = 0;
+int gFail = 0;
+
+// ======== unit tests ==========
+
+bool 
+
+// ======== main function =======
+void main(void){
+    
+}
