c44dcc85fbe4fdcc420003f5776049e9b4da684e
kuolu <kuolu1900@gmail.com>
2017-02-19 23:07:35 -0500

    fixed typo in xml file

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -18,13 +18,17 @@ using namespace tinyxml2;
 using namespace std;
 
 #define outputfile_name "output.h5"
-#define num_of_variables 4
+#define num_of_variables 7
 
 //------------------- assign constants to default values ----------------------
 string method;                      // name of the calculation method
 double radius = RADIUS;             // radius of the tube
+double time_length = TIME_LENGTH;
 double thickness = THICKNESS;       // thickness of the tube
 double height = HEIGHT;             // height of the tube
+double tempreture = 1200.0;
+double conductivity = 1.2;
+double init_temperature = 100.0;
 double dr = DR;                     // infinitesimal width step
 double dz = DZ;                     // infinitesimal height step
 double dt = DT;                     // infinitesimal time step, dr^2 >= 4*dt
@@ -34,14 +38,20 @@ double r = R;
 double mc = MC;
 double qc = QC;
 
-array<string,num_of_variables> variable_names = { "time_length",
+array<const char* ,num_of_variables> variable_names = { "time_length",
                                                   "radius",
                                                   "thickness",
-                                                  "height"};
-array<const double,num_of_variables> variable_values = { TIME_LENGTH,
-                    RADIUS,
-                    THICKNESS,
-                    HEIGHT};
+                                                  "height",
+                                                  "tempreture",
+                                                  "conductivity",
+                                                  "init_temperature"};
+array< double ,num_of_variables> variable_values = { time_length,
+                    radius,
+                    thickness,
+                    height,
+                    tempreture,
+                    conductivity,
+                    init_temperature};
 // read input file function
 void readXMLFile(char* in_name)
 {
@@ -50,18 +60,19 @@ void readXMLFile(char* in_name)
     XMLElement* calcElement = doc.FirstChildElement( "calculation" );
     method =  calcElement -> Attribute("method");
     const char* type;
+    const char* isDefault = "default";
     for (int i = 0; i < num_of_variables; i++){
       printf("hello from for loop\n");
-      // type = doc.FirstChildElement( "calculation" )
-      //     ->FirstChildElement(variable_names[i])-> Attribute("method");
-      // if(type != const char* ("default") {
-      //   variable_values[i] = const_cast<const double>(stod(doc.FirstChildElement( "calculation" )
-      //     ->FirstChildElement(variable_names[i])->GetText()));
-      // }
+      type = doc.FirstChildElement( "calculation" )
+          ->FirstChildElement(variable_names[i])-> Attribute("method");
+      if(type != isDefault) {
+        variable_values[i] = stod(doc.FirstChildElement( "calculation" )
+          ->FirstChildElement(variable_names[i])->GetText());
+      }
     }
 
     for (int i = 0; i < num_of_variables; i++){
-      printf("variable %s has value %f \n",variable_names[i].c_str(),variable_values[i]);
+      printf("variable %s has value %f \n",variable_names[i],variable_values[i]);
     }
 }
 
